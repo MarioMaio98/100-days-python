dcde6363e4331b125296c1e18a0c3afdbb486175
Mario maio <57944979+MarioMaio98@users.noreply.github.com>
2024-07-30 14:19:51 +0000

    basic exercise completed

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print ("Click Run \n to start the game")
+print ("Welcome to the band name generator!")
+name = input("What's your pet name?\n")
+print ("The name of your pet is " + name)
+names = input('where were you born?\n')
+print ('the name of your band could be ' + name + ' ' + names + '!')
+
+
